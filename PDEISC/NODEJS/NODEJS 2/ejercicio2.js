4837a42feec0b494756922cb48e1d95222255b7e
FrancogonzalezFIdel <132099518+FranGFJ@users.noreply.github.com>
2025-04-14 17:40:50 -0600

    Update ejercicio2.js

--- PDEISC/NODEJS/NODEJS 2/ejercicio2.js
+++ PDEISC/NODEJS/NODEJS 2/ejercicio2.js
@@ -1,32 +1,34 @@
 import{ calcularPromedio, calcularPorcentaje, perimetro } from './modulo1.js';
-import { createServer } from 'node:http';
 
-
-
-const server = createServer((req, res) => {
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
   fs.readFile('ejercicio2.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end(`
-   
-      <table>
-        <tbody>
-          <tr>
-            <td>PROMEDIO: </td>
-            <td>${calcularPromedio(7, 10, 8)}</td>
-          </tr>
-          <tr>
-            <td>PORCENTAJE</td>
-            <td>${calcularPorcentaje(25, 150)}</td>
-          </tr>
-          <tr>
-            <td>PERIMETRO:</td>
-            <td>${perimetro(5,15)}</td>
-          </tr>
-        </tbody>
-      </table>
-  `);
-})
+    <table>
+      <tbody>
+        <tr>
+          <td>PROMEDIO: </td>
+          <td>${calcularPromedio(7, 10, 8)}</td>
+        </tr>
+        <tr>
+          <td>PORCENTAJE</td>
+          <td>${calcularPorcentaje(25, 150)}</td>
+        </tr>
+        <tr>
+          <td>PERIMETRO:</td>
+          <td>${perimetro(5,15)}</td>
+        </tr>
+      </tbody>
+    </table>
+`);
+  });
+});
+
+server.listen(8080, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:8080');
 });
 
 
