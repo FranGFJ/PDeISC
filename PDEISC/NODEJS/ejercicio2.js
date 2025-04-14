45837d39ac75dd8ccb37ce6568da69d09213c0cf
FrancogonzalezFIdel <132099518+FranGFJ@users.noreply.github.com>
2025-04-14 17:39:22 -0600

    Add files via upload

---
+++ PDEISC/NODEJS/ejercicio2.js
@@ -0,0 +1,32 @@
+import{ calcularPromedio, calcularPorcentaje, perimetro } from './modulo1.js';
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('ejercicio2.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end(`
+    <table>
+      <tbody>
+        <tr>
+          <td>PROMEDIO: </td>
+          <td>${calcularPromedio(7, 10, 8)}</td>
+        </tr>
+        <tr>
+          <td>PORCENTAJE</td>
+          <td>${calcularPorcentaje(25, 150)}</td>
+        </tr>
+        <tr>
+          <td>PERIMETRO:</td>
+          <td>${perimetro(5,15)}</td>
+        </tr>
+      </tbody>
+    </table>
+`);
+  });
+});
+
+server.listen(8080, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:8080');
+});
